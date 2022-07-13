5fe309db869a84627cd32e0a5d8d80321fb3f280
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-12 23:46:28 -0500

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
-import Home from './routes/Home';
-
+// import {Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+// import Home from './routes/Home';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>   
-    </>
+    <div>
+      <NavBar/>
+    </div>
   );
 }
 
